27ab5994308a124ce0a294d0f60ffc4efa3d368d
Maxim Litvinskiy <litvinskiy.maksimka8@gmail.com>
2025-12-22 20:46:34 +0300

    les8 HW

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace AnimalProgram
+{
+    
+    abstract class Animal
+    {
+        public string Name { get; set; }
+
+        
+        public void SetName(string name)
+        {
+            Name = name;
+        }
+
+        
+        public string GetName()
+        {
+            return Name;
+        }
+
+       
+        public abstract void Eat();
+    }
+
+    
+    class Dog : Animal
+    {
+        
+        public override void Eat()
+        {
+            Console.WriteLine($"{Name} ест корм.");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Dog dog = new Dog();
+
+            
+            Console.WriteLine("Введите имя собаки:");
+            string dogName = Console.ReadLine();
+
+            
+            dog.SetName(dogName);
+
+            
+            Console.WriteLine($"Имя собаки: {dog.GetName()}");
+
+            
+            dog.Eat();
+
+            
+            Console.WriteLine("\nНажмите любую клавишу для выхода...");
+            Console.ReadKey();
+        }
+    }
+}
